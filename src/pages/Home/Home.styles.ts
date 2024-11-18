3921c19f041797027a6d0de8d0d345e953678bd6
Rolando L Gonzales <rolandolloyd@gmail.com>
2024-11-18 06:29:15 -0500

    Deleted comments

--- src/pages/Home/Home.styles.ts
+++ src/pages/Home/Home.styles.ts
@@ -1,4 +1,5 @@
 // src/pages/Home/Home.styles.ts
+
 import styled from 'styled-components';
 
 export const HomeContainer = styled.div`
@@ -28,35 +29,3 @@ export const ImageDiv = styled.div`
       }
    }
 `;
-
-// // src/pages/Home/Home.styles.ts
-// import styled from 'styled-components';
-
-// export const HomeContainer = styled.div`
-//    display: flex;
-//    flex-direction: column;
-//    justify-content: center;
-//    align-items: center;
-//    min-height: 2040px; // Double the scroll height to ensure enough space
-//    padding-top: ${({ theme }) => theme.sizes.navHeight};
-//    position: relative;
-// `;
-
-// export const ImageDiv = styled.div`
-//    position: fixed;
-//    bottom: 50px;
-//    display: flex;
-//    justify-content: center;
-//    align-items: center;
-//    width: 100%;
-
-//    img {
-//       width: 900px;
-//       object-fit: contain;
-
-//       @media (max-width: 768px) {
-//          width: 110vw;
-//          bottom: 150px;
-//       }
-//    }
-// `;
